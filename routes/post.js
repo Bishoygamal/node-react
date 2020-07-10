cd2cd789b2ce9c886a18c4ca9800081c6511d2af
Bisho Gamal <beshoy.ot@gmail.com>
2020-07-10 10:09:01 +0200

    finishing API's

--- routes/post.js
+++ routes/post.js
@@ -1,17 +1,23 @@
 const express = require("express");
-const { getPosts, createPost } = require("../controllers/post");
+const { getPosts, createPost, postsByUser ,postById,isPoster,deletePost, updatePost } = require("../controllers/post");
 const { userById } = require("../controllers/users");
 const { requiresSignin } = require("../controllers/auth");
 const validator = require("../validator/index");
 const router = express.Router();
 
 router.get("/", getPosts);
-//router.post('/post',requiresSignin,validator.createPostValidator, createPost)
+
 router.post(
   "/post/new/:userId",
   requiresSignin,
   createPost,
   validator.createPostValidator
 );
+
+router.get("/posts/by/:userId", requiresSignin, postsByUser);
+router.put("/post/:postId",requiresSignin,isPoster,updatePost)
+router.delete("/post/:postId",requiresSignin,isPoster,deletePost)
+
 router.param("userId", userById);
+router.param("postId", postById);
 module.exports = router;
